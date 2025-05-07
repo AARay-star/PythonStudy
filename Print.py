9c717901e56356e6681e256f7b9d2c3917d6bdbf
Raymond <1250374437@qq.com>
2025-05-07 12:07:08 +0800

    print

---
+++ Print.py
@@ -0,0 +1,34 @@
+# 学习Python GOGOGO
+
+
+# 第一个注释
+print("Hello World")  #打印输出
+
+'''
+多行注释方法一
+多行注释方法一
+'''
+
+"""
+多行注释方法二
+多行注释方法二
+"""
+
+# 用严格的换行来替代{}等信息
+if 0:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+    print ("False")
+
+# 可以利用反斜杠 \来表示换行
+item_one = 1
+item_two = 2
+item_three = 3
+total = item_one + \
+        item_two + \
+        item_three
+print(total) # 输出为 6
+
+
